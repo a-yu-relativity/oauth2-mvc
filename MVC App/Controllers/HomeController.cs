a773eec634927df9cd646286b265275f6155cc8e
Albert Yu <albert.yu@relativity.com>
2018-08-20 11:40:28 -0500

    Add query string building

--- MVC App/Controllers/HomeController.cs
+++ MVC App/Controllers/HomeController.cs
@@ -10,7 +10,26 @@ namespace MVC_App.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            const string AUTH_URL = "https://p-dv-vm-two7bin.kcura.corp/Relativity/Identity/connect/authorize";
+            const string CLIENT_ID = "2b28a80c61ea80a08a5e7a7e24";
+            const string CLIENT_SECRET = "52a2e31b9c461e6a8ec874444fda5ce6a68b72ef";
+            const string SCOPE = "UserInfoAccess";
+            const string REDIRECT_URI = "http://localhost:49203/";
+            const string GRANT_TYPE = "code";
+
+            var uriBuilder = new UriBuilder(AUTH_URL);
+            System.Collections.Specialized.NameValueCollection query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            query["client_id"] = CLIENT_ID;
+            query["client_secret"] = CLIENT_SECRET;
+            query["scope"] = SCOPE;
+            query["redirect_uri"] = REDIRECT_URI;
+            query["grant_type"] = GRANT_TYPE;
+
+            uriBuilder.Query = query.ToString();
+            string finalUrl = uriBuilder.ToString();
+
+            //return View();
+            return Redirect(finalUrl);
         }
 
         public ActionResult About()
