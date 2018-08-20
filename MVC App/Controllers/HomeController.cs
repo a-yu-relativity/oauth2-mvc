d318d941fa4403557b6bf6ef89186c35356d5351
Albert Yu <albert.yu@relativity.com>
2018-08-20 15:35:59 -0500

    Move constants to class variables

--- MVC App/Controllers/HomeController.cs
+++ MVC App/Controllers/HomeController.cs
@@ -8,16 +8,21 @@ namespace MVC_App.Controllers
 {
     public class HomeController : Controller
     {
+        private const string AUTH_URL = "https://p-dv-vm-two7bin.kcura.corp/Relativity/Identity/connect/authorize";
+        private const string TOKEN_URL = "https://p-dv-vm-two7bin.kcura.corp/Relativity/Identity/connect/token";
+        private const string CLIENT_ID = "2b28a80c61ea80a08a5e7a7e24";
+        private const string CLIENT_SECRET = "52a2e31b9c461e6a8ec874444fda5ce6a68b72ef";
+        private const string SCOPE = "UserInfoAccess";
+        private const string REDIRECT_URI = "http://localhost:49203";
+        private const string GRANT_TYPE = "code";
+
+
+        private string _bearerToken = String.Empty;
+
+        private string _refreshToken = String.Empty;
 
         private string GetRedirectUri()
         {
-            const string AUTH_URL = "https://p-dv-vm-two7bin.kcura.corp/Relativity/Identity/connect/authorize";
-            const string CLIENT_ID = "2b28a80c61ea80a08a5e7a7e24";
-            const string CLIENT_SECRET = "52a2e31b9c461e6a8ec874444fda5ce6a68b72ef";
-            const string SCOPE = "UserInfoAccess";
-            const string REDIRECT_URI = "http://localhost:49203";
-            const string GRANT_TYPE = "code";
-
             var uriBuilder = new UriBuilder(AUTH_URL);
             System.Collections.Specialized.NameValueCollection query = HttpUtility.ParseQueryString(uriBuilder.Query);
             query["client_id"] = CLIENT_ID;
@@ -33,12 +38,36 @@ namespace MVC_App.Controllers
         }
 
 
+        /// <summary>
+        /// Extracts the query string param from the query string
+        /// e.g. GetParam("https://thing?foo=bar", "foo") => "bar"
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private string GetParam(string url, string param)
+        {
+            var uriBuilder = new UriBuilder(url);
+            System.Collections.Specialized.NameValueCollection query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            string result = query[param];
+            return result;
+        }
+
+
         public ActionResult Index()
         {
-            string finalUrl = GetRedirectUri();
+            if (String.IsNullOrEmpty(_bearerToken))
+            {
+                string finalUrl = GetRedirectUri();
+                return Redirect(finalUrl);
+            }
 
-            //return View();
-            return Redirect(finalUrl);
+            return View();
+        }
+
+
+        public ActionResult Redirect()
+        {
+            return View();
         }
 
         public ActionResult About()
